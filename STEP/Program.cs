7d78ffd242199d1e95b5da93a05e8f076f5596d3
KarmaKamikaze <nicolaijgn@gmail.com>
2022-03-31 12:45:31 +0200

    Added project files to prepare for AST construction

---
+++ STEP/Program.cs
@@ -0,0 +1,36 @@
+﻿using STEP.lexer;
+using STEP.parser;
+
+namespace STEP;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length != 1)
+            Exit("Usage: STEP.exe filename");
+
+        using (StreamReader streamReader = new StreamReader(File.Open(args[0], FileMode.Open)))
+        {
+            // Read the source code file
+            Lexer lexer = new Lexer(streamReader);
+            
+            // Parse the source code
+            Parser parser = new Parser(lexer);
+        }
+        
+        Exit("Finished!");
+    }
+
+    private static void Exit(string? message)
+    {
+        if (message != null)
+            Console.WriteLine(message);
+        else
+            Console.WriteLine();
+
+        Console.WriteLine("Press any key to exit...");
+        Console.Read();
+        Environment.Exit(0);
+    }
+}
