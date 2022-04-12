d3c498665facb20315b4a15898f600ab6668cb60
KarmaKamikaze <nicolaijgn@gmail.com>
2022-04-12 14:06:30 +0200

    Node directory to contains all node classes

---
+++ STEP/AST/Nodes/NodeFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace STEP.AST;
+
+public class NodeFactory
+{
+    public static AstNode MakeNode(AstNodeType type)
+    {
+        switch (type)
+        {
+            // Expressions
+            case AstNodeType.IdNode:
+                return new IdNode();
+            case AstNodeType.PlusNode:
+                return new PlusNode();
+            case AstNodeType.MinusNode:
+                return new MinusNode();
+            case AstNodeType.MultNode:
+                return new MultNode();
+            case AstNodeType.AssNode:
+                return new AssNode();
+            default:
+                return new NullNode();
+        }
+    }
+}
