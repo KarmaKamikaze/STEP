050265d2da41da244579cbc8b1a275086a52d5b9
WassawRoki <56611129+WassawRoki@users.noreply.github.com>
2022-04-13 16:00:26 +0200

    Create SymbolTable.cs

---
+++ STEP/AST/SymbolTable.cs
@@ -0,0 +1,93 @@
+namespace STEP.AST.Nodes;
+//THe Symbol Table for the AST
+
+class SymTableEntry
+{
+    public string symbol_name { get; set; }
+    public TypeVal Type { get; set; }
+}
+
+public interface ISymbolTable {
+    void OpenScope();
+    void CloseScope();
+    SymTableEntry RetriveSymbol(string id);
+    void EnterSymbol(SymTableEntry sym);
+    bool IsDeclaredLocally(string id);
+}
+
+public class SymbolTable
+{
+    private Dictionary<string, List<SymTableEntry>> symbolDict = new();
+    private Stack<Dictionary<string, SymTableEntry>> scopeStack = new();
+    private int depth = 0;
+
+    public void OpenScope()
+    {
+        this.depth++;
+        scopeStack.Push(new SymbolTable());
+    }
+
+    public void CloseScope()
+    {
+        var prevSymTable = scopeStack.Pop();
+        foreach(var sym in prevSymTable) 
+        {
+            
+        }
+        
+        SymTableEntry previousSymbol;
+        foreach(var symbol in scopeDisplay[depth])
+        {
+            previousSymbol = symbol.var;
+            Delete(symbol);
+            if (previousSymbol != null)
+            {
+                Add(previousSymbol);
+            }
+        }
+        this.depth--;
+    }
+    
+    private void Add(SymTableEntry sym) 
+    {
+        if (tableDict.GetValueOrDefault(sym.var) is List<SymTableEntry> collisionChain)
+        {
+            collisionChain.Add(sym);
+        }
+    }
+
+    private void Delete(SymTableEntry sym)
+    {
+        if (tableDict.GetValueOrDefault(sym.var) is List<SymTableEntry> collisionChain)
+        {
+            collisionChain.Remove(sym);
+        }
+    }
+    public SymTableEntry RetriveSymbol(string id)
+    {
+        //symbol = HashTable.get(name)
+        //while sumbol not null do
+            //if symbol.name == name
+                //then return symbol
+                //symbol = symbol.hash
+        return null;
+    }
+
+    public void EnterSymbol(SymTableEntry sym)
+    {
+        // Ét symbol table: 
+        // - Level som attribute på SymTableEntry
+        // - Values i vores Dictionary er en liste af SymTableEntries med forskellige scopes (og samme id)
+        
+        // ét symbol table pr. level:
+        // - 
+    }
+
+    public IsDeclaredLocally(string id)
+    {
+        //if declaredlocally
+            //return true
+        //elif
+            //return false
+    }
+}
