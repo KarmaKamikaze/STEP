6766a3720399c00c96f56e384f2a4eafe574cfb5
KarmaKamikaze <nicolaijgn@gmail.com>
2022-04-20 15:16:22 +0200

    Remove NullNode class

--- STEP/AST/Nodes/NullNode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace STEP.AST.Nodes;
-
-public class NullNode : AstNode
-{
-    // This is a null node to represent absence in the AST.
-    public override void Accept(IVisitor v) {
-        v.Visit(this);
-    }
-}
