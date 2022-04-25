836863c4c031860943cd4420de2047cc06e0f754
Nikolaj Dam <ndam20@student.aau.dk>
2022-04-25 14:11:07 +0200

    Created CodeGnerationVisitorTests.cs

---
+++ StepTests/CodeGenerationVisitorTests.cs
@@ -0,0 +1,17 @@
+﻿using STEP.CodeGeneration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StepTests;
+
+public class CodeGenerationVisitorTests
+{
+    private readonly CodeGenerationVisitor _visitor = new();
+
+
+}
+
