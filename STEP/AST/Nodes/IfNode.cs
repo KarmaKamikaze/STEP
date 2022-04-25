4e51f985815b926d9e6a7b22b93e5e999136b233
Nikolaj Dam <ndam20@student.aau.dk>
2022-04-25 10:15:29 +0200

    Simplified Equals method in IfNode

--- STEP/AST/Nodes/IfNode.cs
+++ STEP/AST/Nodes/IfNode.cs
@@ -16,56 +16,12 @@ public class IfNode : StmtNode
 
     public override bool Equals(object obj)
     {
-        if(obj is IfNode other) 
+        if(obj is IfNode other)
         {
-
-            //TODO: fix this shit with SequenceEquals
-            // Check if all parts of the if nodes are equal
-            if(!Equals(other.Condition, Condition))
-            {
-                return false;
-            }
-
-            // ThenClause - check if they have the same number of statements
-            if (other.ThenClause.Count != ThenClause.Count)
-            {
-                return false;
-            }
-            // If so, check if each one is identical
-            for (int i = 0; i < ThenClause.Count; i++)
-            {
-                if (!Equals(other.ThenClause[i], ThenClause[i]))
-                {
-                    return false;
-                }
-            }
-
-            // ElseIfClauses
-            if (other.ElseIfClauses.Count != ElseIfClauses.Count)
-            {
-                return false;
-            }
-            for (int i = 0; i < ElseIfClauses.Count; i++)
-            {
-                if (!Equals(other.ElseIfClauses[i], ElseIfClauses[i]))
-                {
-                    return false;
-                }
-            }
-
-            // ElseClause
-            if (other.ElseClause.Count != ElseClause.Count)
-            {
-                return false;
-            }
-            for (int i = 0; i < ElseClause.Count; i++)
-            {
-                if (!Equals(other.ElseClause[i], ElseClause[i]))
-                {
-                    return false;
-                }
-            }
-            return true;
+            return Equals(other.Condition, Condition)
+                   && other.ThenClause.SequenceEqual(ThenClause)
+                   && other.ElseIfClauses.SequenceEqual(ElseIfClauses)
+                   && other.ElseClause.SequenceEqual(ElseClause);
         }
         return false;
     }
