a1967c327a20b49629e0a8059060383468c2bdac
Sarmisuper <sarmilan3@gmail.com>
2022-04-26 14:40:46 +0200

    created file things

--- StepTests/IntegrationTests.cs
+++ StepTests/IntegrationTests.cs
@@ -1,17 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
 using Antlr4.Runtime;
 using STEP;
 using STEP.AST;
 using STEP.AST.Nodes;
 using STEP.CodeGeneration;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace StepTests;
 
 public class IntegrationTests
 {
-    private AstBuilderVisitor _astBuilder = new();
-    
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    private static readonly bool IsRunningFromXUnit = AppDomain.CurrentDomain.GetAssemblies()
+        .Any(assembly => assembly.FullName.ToLowerInvariant().StartsWith("xunit"));
+
+    public IntegrationTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     private STEPParser.ProgramContext GetParseTree(string input) 
     { 
         AntlrInputStream streamReader = new(input);
@@ -20,17 +35,19 @@ public class IntegrationTests
         STEPParser parser = new(tokenStream);
         return parser.program();
     }
-
+    
     [Theory]
     [MemberData(nameof(programStrings))]
-    public void IntegrationTest1(string source, string expected)
+    public void IntegrationTest1(string sourceFile, string expectedFile)
     {
-        STEPParser.ProgramContext parseTree = GetParseTree(source);
+        string sourceFilePath = Directory.GetCurrentDirectory() + "\\..\\..\\..\\" + sourceFile;
+        string sourceText = File.ReadAllText(sourceFilePath);
+        STEPParser.ProgramContext tree = GetParseTree(sourceText);
         
         AstBuilderVisitor astBuilder = new AstBuilderVisitor();
-        AstNode root = astBuilder.Build(parseTree);
+        AstNode root = astBuilder.Build(tree);
         
-        TypeVisitor typeVisitor = new();
+        TypeVisitor typeVisitor = new TypeVisitor();
         root.Accept(typeVisitor);
         
         CodeGenerationVisitor codeGen = new CodeGenerationVisitor();
@@ -38,28 +55,19 @@ public class IntegrationTests
 
         string actual = codeGen.OutputToString();
 
+        string expectedFilePath = Directory.GetCurrentDirectory() + "\\..\\..\\..\\" + expectedFile;
+        string expected = File.ReadAllText(expectedFilePath);
+        
         Assert.Equal(expected, actual);
     }
 
     public static IEnumerable<string[]> programStrings =>
         new List<string[]>()
         {
-            new string[] {@"setup
-                           if(false)
-                           else
-                             if(true)
-                             end if
-                           end if
-                         end setup",
-                           "void setup() {\r\n"+ 
-                             "if(false) {\r\n"+
-                             "}\r\n"+
-                             "else {\r\n"+
-                                "if(true) {\r\n"+
-                                "}\r\n"+
-                             "}\r\n"+
-                           "}\r\n" 
-            },
+            new string[]
+            {
+                "IntegrationTestPrograms/test.step" , "IntegrationTestPrograms/test2.step"  
+            }
             
         };
 
