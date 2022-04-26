8ac75a67491f3604367832025e1fe631a3d117aa
Mikkel <mstids20@student.aau.dk>
2022-04-26 10:39:36 +0200

    Cleaned up typevisitor, added AstNode property to type exception

--- STEP/AST/TypeException.cs
+++ STEP/AST/TypeException.cs
@@ -3,8 +3,23 @@
 namespace STEP.AST; 
 
 public class TypeException : Exception {
-    public TypeException(string s) : base(s) {}
-    public TypeException(TypeVal a1, TypeVal a2) : base($"Type mismatch, expected matching types - actual types: ({a1}, {a2})") { }
-    public TypeException(TypeVal expected, TypeVal a1, TypeVal a2) : base($"Type mismatch, expected types: ({expected}, {expected}) - actual types: ({a1}, {a2})") { }
-    public TypeException(TypeVal expected1, TypeVal expected2, TypeVal a1, TypeVal a2) : base($"Type mismatch, allowed types: {expected1} or {expected2} - actual types: ({a1}, {a2})") { }
+    public TypeException(AstNode n, string s) : base(s) {
+        ErrorNode = n;
+    }
+
+    public TypeException(AstNode n, TypeVal a1, TypeVal a2) : base(
+        $"Type mismatch, expected matching types - actual types: ({a1}, {a2})") {
+        ErrorNode = n;
+    }
+
+    public TypeException(AstNode n, TypeVal expected, TypeVal a1, TypeVal a2) : base(
+        $"Type mismatch, expected types: ({expected}, {expected}) - actual types: ({a1}, {a2})") {
+        ErrorNode = n;
+    }
+
+    public TypeException(AstNode n, TypeVal expected1, TypeVal expected2, TypeVal a1, TypeVal a2) : base(
+        $"Type mismatch, allowed types: {expected1} or {expected2} - actual types: ({a1}, {a2})") {
+        ErrorNode = n;
+    }
+    public AstNode ErrorNode { get; set; }
 }
