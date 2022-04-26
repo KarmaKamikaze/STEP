f1591a8c9d0fde0d318d1e6086981896caf448d5
Sarmisuper <sarmilan3@gmail.com>
2022-04-26 10:10:24 +0200

    started tests of for nodes

--- StepTests/CodeGenerationVisitorTests.cs
+++ StepTests/CodeGenerationVisitorTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using STEP.AST.Nodes;
 using Xunit;
 
 namespace StepTests;
@@ -16,7 +17,21 @@ public class CodeGenerationVisitorTests
     [Fact]
     public void ForNodeInitializerIsCreatedCorrectly()
     {
-        const string expected = "for()";
+        //Arrange
+        const string expected = "for(double i = 0; i <= 10; i = i + 1) {\n}";
+        IdNode idNode = new IdNode() { Id = "i", Type = new STEP.Type() { ActualType = TypeVal.Number}};
+        NumberNode exprNode = new NumberNode() { Value = 0, Type = new STEP.Type() { ActualType = TypeVal.Number }};
+        VarDclNode varInit = new VarDclNode() { Left = idNode, Right = exprNode, Type = new STEP.Type() { ActualType = TypeVal.Number}};
+        
+        NumberNode limitNode = new NumberNode() { Value = 10, Type = new STEP.Type() { ActualType = TypeVal.Number }};
+        NumberNode updateNode = new NumberNode() { Value = 1, Type = new STEP.Type() { ActualType = TypeVal.Number }};
+
+        ForNode forNode = new ForNode() { Initializer = varInit, Limit = limitNode, Update = updateNode};
+        
+        //Act
+        _visitor.Visit(forNode);
+        string actual = _visitor.O
+
 
     }
     
