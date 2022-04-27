781abd23739ce9a89f2418c6a4b8e5c6ee408509
Sarmisuper <sarmilan3@gmail.com>
2022-04-27 10:26:45 +0200

    dunno

--- StepTests/IntegrationTestPrograms/int2Expected.ino
+++ StepTests/IntegrationTestPrograms/int2Expected.ino
@@ -5,17 +5,23 @@ void setup() {
 pinMode(INPUT);
 
 str = "test2";
-double num2 = 1;
+double num2 = 0;
+for(double i = num2; i <= num; i = i + 3) {
+num2 = num2 + 1;
+}
+}
+void loop() {
+double num3 = 1;
 while(true) {
-if(num2 > 20) {
+if(num3 > 20) {
 break;
 }
-else if(num2 > num) {
-num2 = num2 * 2;
+else if(num3 > num) {
+num3 = num3 * 2;
 continue;
 }
 else {
-num2 = num2 + 1;
+num3 = num3 + 1;
 }
 }
 }
